52499d3660c6ebee855c65cfd19b27cea45c92c5
David Isebarn <isebarn182@gmail.com>
2021-09-14 20:12:09 +0000

    first

---
+++ ORM.py
@@ -0,0 +1,85 @@
+from sqlalchemy import create_engine
+from sqlalchemy import func
+from sqlalchemy import (
+    ForeignKey,
+    desc,
+    create_engine,
+    func,
+    Column,
+    BigInteger,
+    JSON,
+    Integer,
+    Float,
+    String,
+    Boolean,
+    DateTime,
+    Text,
+)
+
+from datetime import datetime
+from sqlalchemy import desc
+
+import os
+import json
+
+connectionString = os.environ.get('CAR_DATABASE')
+
+engine = create_engine(connectionString, echo=False)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
+
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    Id = Column(Integer, primary_key=True)
+    Driven = Column(Integer)
+    Fuel = Column(String)
+    Doors = Column(Integer)
+    Valves = Column(Integer)
+    Seats = Column(Integer)
+    Maker = Column(String)
+    Drivetrain = Column(String)
+    Transmission = Column(String)
+    Color = Column(String)
+    Inspected = Column(Boolean)
+    Model = Column(String)
+    Year = Column(Integer)
+
+    def __init__(self, data):
+        self.Id = data.get('_id')
+
+        self.Driven = int(data.get('Akstur', '0').replace(".", ''))
+        self.Fuel = data.get('Eldsneyti', '')
+        self.Doors = int(data.get('Fjöldi dyra', '0'))
+        self.Valves = int(data.get('Fjöldi strokka', '0'))
+        self.Seats = int(data.get('Fjöldi sæta', '0'))
+        self.Maker = data.get('Framleiðandi', '')
+        self.Drivetrain = data.get('Hjóladrifin', '')
+        self.Color = data.get('Litur', '')
+        self.Transmission = data.get('Skipting', '')
+        self.Inspected = data.get('Skoðaður', '') == 'Já'
+        self.Model = data.get('Undirtegund', '')
+        self.Year = int(data.get('Ár', '0'))
+
+
+class Operations:
+    def SaveCar(car):
+        session.add(car)
+        session.commit()
+
+    def SavedIds():
+        return [x[0] for x in session.query(Car.Id).all()]
+
+
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
---
+++ bland/spiders/spider.py
@@ -0,0 +1,45 @@
+import scrapy
+from ORM import Operations
+from ORM import Car
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "cars"
+    url = "https://bland.is/classified/entry.aspx?classifiedId={}"
+    featuredPropWrapper = ".//div[@class='featuredPropertyWrapper']/div"
+    propKey = ".//div[@class='featuredPropertyKey']/text()"
+    propVal = ".//div[@class='featuredPropertyValue']/text()"
+
+    def start_requests(self):
+        url = "https://bland.is/solutorg/farartaeki/nyir-notadir-bilar-til-solu/?categoryId=17&sub=1&page={}"
+        self.saved_ids = Operations.SavedIds()
+
+        for page in range(0, 30):
+            yield scrapy.Request(url=url.format(page), callback=self.parse_page)
+
+    def parse_page(self, response):
+        vehicle_ids = [
+            int(x.extract().split("=")[-1])
+            for x in response.xpath('//div[@data-url]/@data-url')
+        ]
+
+        new_ids = list(set(vehicle_ids) - set(self.saved_ids))
+        from pprint import pprint
+
+        for new_id in new_ids:
+            yield scrapy.Request(
+                url=self.url.format(new_id),
+                callback=self.parse_car,
+                meta={'_id': new_id},
+            )
+
+    def parse_car(self, response):
+        values = [
+            x.extract() for x in response.xpath("//table")[0].xpath(".//td/text()")
+        ]
+        values = {values[x]: values[x + 1] for x in range(0, len(values), 2)}
+        from pprint import pprint
+
+        values['_id'] = response.meta.get('_id')
+
+        car = Operations.SaveCar(Car(values))
